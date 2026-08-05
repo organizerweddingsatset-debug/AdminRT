78e0b928846a23f67ad27952cb82ac1aeb0fa236
organizerweddingsatset-debug <organizerweddingsatset@gmail.com>
2026-08-05 12:04:17 +0700

    Delete lib directory

--- lib/supabase.ts
+++
@@ -1,7 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js'
-import { createBrowserClient as createSSRBrowserClient } from '@supabase/ssr'
-const url = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-export const supabase = createClient(url, anon)
-export function createBrowserClient(){ return createSSRBrowserClient(url, anon) }
