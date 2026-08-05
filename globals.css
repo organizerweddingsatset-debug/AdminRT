093187964378d92eb849be501fac02863c6f2824
organizerweddingsatset-debug <organizerweddingsatset@gmail.com>
2026-08-05 11:48:11 +0700

    Delete globals.css

--- globals.css
+++
@@ -1,6 +0,0 @@
-
-import './globals.css'
-export const metadata={title:'RT 09/14 De Naila Village'}
-export default function RootLayout({children}:{children:React.ReactNode}){
-  return <html lang="id"><body className="antialiased">{children}</body></html>
-}
